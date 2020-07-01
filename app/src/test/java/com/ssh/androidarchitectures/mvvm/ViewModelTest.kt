901c335c1f33a7057dfadb968832650e4a13aaf4
sabbirshiam <sabbirshiam@gmail.com>
2020-07-01 20:23:41 +0900

    unit test MVVM.

--- app/src/test/java/com/ssh/androidarchitectures/mvvm/ViewModelTest.kt
+++ app/src/test/java/com/ssh/androidarchitectures/mvvm/ViewModelTest.kt
@@ -1,5 +1,7 @@
 package com.ssh.androidarchitectures.mvvm
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.ssh.androidarchitectures.BaseSchedulerProvider
 import com.ssh.androidarchitectures.ImmediateSchedulerProvider
 import com.ssh.androidarchitectures.model.Country
@@ -7,18 +9,27 @@ import com.ssh.androidarchitectures.repositories.CountryRepository
 import io.reactivex.Single
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
 class ViewModelTest {
 
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
     @Mock
     private lateinit var repository: CountryRepository
     private lateinit var schedulerProvider: BaseSchedulerProvider
     private val countries = listOf(Country("a"), Country("b"))
     private lateinit var viewModel: CountriesViewModel
+
+    @Mock
+    lateinit var observer: Observer<List<String>>
 
     @Before
     fun setup() {
@@ -42,6 +53,9 @@ class ViewModelTest {
     fun fetchCountriesAndShowIntoView() {
         Mockito.`when`(repository.getCountries()).thenReturn(Single.just(countries))
         viewModel.start()
-        Assert.assertEquals(viewModel.getCountries(), countries)
+        viewModel.getCountries().observeForever(observer)
+        val expected = countries.map { it.name }
+        Assert.assertEquals(viewModel.getCountries().value, expected)
+        verify(observer).onChanged(expected)
     }
 }
