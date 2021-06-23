6af878b4f3f6a0047997897c2befd10e404a7182
sabbirshiam <sabbirshiam@gmail.com>
2021-06-23 14:40:00 -0600

    update gradle version and sdk version to compile.

--- app/src/test/java/com/ssh/androidarchitectures/mvvm/ViewModelTest.kt
+++ app/src/test/java/com/ssh/androidarchitectures/mvvm/ViewModelTest.kt
@@ -17,8 +17,6 @@ import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 
 class ViewModelTest {
-
-
     @Rule
     @JvmField
     val instantTaskExecutorRule = InstantTaskExecutorRule()
